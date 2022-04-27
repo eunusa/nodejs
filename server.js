bf5916c144f14046871f3dbfcd1729f2431b3534
saeunwoo <66991444+eunusa@users.noreply.github.com>
2022-04-27 21:53:48 +0900

    db 삽입 삭제 기능 구현
    ajax 삭제 기능 구현

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express(); // express  소환한다.
-const bodyParser= require('body-parser')
-app.use(bodyParser.urlencoded({extended: true})) 
+const bodyParser= require('body-parser') // form 데이터 서버로 쉽게 전송해 주는 라이브러리
+app.use(bodyParser.urlencoded({extended: true})) //form 데이터 서버로 쉽게 전송해 주는 라이브러리
 app.set('view engine', 'ejs'); //리스트 라이브러리
+
+app.use('/public', express.static('public'));// static을 사용하기위해서 /public 폴더에 넣는다.
+
+
 
 //데이터 입력시 필요함
 const MongoClient = require('mongodb').MongoClient;
@@ -38,18 +42,18 @@ app.get('/beauty',function(요청,응답){
 });
 app.get('/',function(요청,응답){
 
-    응답.sendfile(__dirname +'/index.html')
+    응답.render('index.ejs'); //url로 html을 불러온다.
 
 });
 app.get('/write',function(요청,응답){
 
-    응답.sendfile(__dirname +'/write.html')
+    응답.render('write.ejs');
 
 });
 
 app.post('/add',function(요청,응답){
     응답.send('전송완료')
-    console.log(요청.body.date);
+    console.log(요청.body.date); //body-parser 라이브러리 설치해야지 데이터를 쉽게 처리 가능하다.
     console.log(요청.body.title);
 
     db.collection('counter').findOne({name :"게시물갯수"}, function(에러,결과){
@@ -63,13 +67,16 @@ app.post('/add',function(요청,응답){
             // inc 1을 증가 시켜주세요.
         
     });
-
-    
     }); 
-
-
-
 });
+
+app.delete('/delete',function(요청,응답){
+    요청.body._id = parseInt(요청.body._id); //parseInt로 '1' 정수로 변경해준다. 서버전송시 문자로 하면 안되고 정수로 변경해야된다.
+    db.collection('post').deleteOne(요청.body, function(에러,결과){
+        console.log('삭제완료');
+        응답.status(200).send({message : '성공했습니다'}); // 요청성공하면 200을 출력
+    })
+})
 
 
 app.get('/list',function(요청,응답){
@@ -82,3 +89,12 @@ app.get('/list',function(요청,응답){
     //ejs파일은 항상 views에 넣어둔다.
 
 })
+
+app.get('/detail/:id',function(요청,응답){ // :를 붙이면 사용자가 /뒤에 ??붙으면 실행한다. parameter.
+    
+     db.collection('post').findOne({_id : parseInt(요청.params.id)},function(에러,결과){ //디비에서 id값 게시물을 찾는다.
+        응답.render('detail.ejs',{data :결과});  //찾은 결과를 ejs에 데이터값을 넣는다.
+        console.log(결과);
+              
+    })});
+
